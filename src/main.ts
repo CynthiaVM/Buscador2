33e2de6d28b7c01633d3fb429783abb893dfd318
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-05-21 19:21:28 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import {} from './productos/productos';
+import { Producto } from './productos/productos.interfaces';
+
 import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -10,11 +13,28 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `;
 
 const btnBuscar = document.querySelector<HTMLInputElement>('#buscar-btn')!;
-const resultado = document.querySelector<HTMLDivElement>('#resultado')!;
+
 btnBuscar.addEventListener('click', (e) => {
-	const producto = document.querySelector<HTMLInputElement>('#buscar-input')!;
-	e.preventDefault();
+  e.preventDefault();
+  //se capturado el valor ingresado por el usuario en la variable inputValue dentro del evento click del botón de búsqueda.
+  const inputValue = document.querySelector<HTMLInputElement>('#buscar-input')!.value;
+
+  const resultado = buscarPorNombre(inputValue);
+  console.log(resultado);
+});
+
+const productos: Producto[] = [
+  { id: 1, nombre: 'Producto 1', precio: 10.99 },
+  { id: 2, nombre: 'Producto 2', precio: 20.99 },
+  { id: 3, nombre: 'Producto 3', precio: 30.99 },
+];
 
 	//TODo: llamar a la funcion de buscarPorNombre(inpurtValue) con el valor que el usuario ingresio
 	// e imprimir en consola el resultado
-});
+
+  function buscarPorNombre(inputValue: string): Producto[] {
+    const resultados: Producto[] = productos.filter((producto) =>
+      producto.nombre.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    return resultados;
+  }
