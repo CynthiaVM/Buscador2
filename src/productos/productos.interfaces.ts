9559371404d6ff2805234cd283f7ea6e47ad6a1e
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-05-21 19:21:32 -0300

    Update productos.interfaces.ts

--- src/productos/productos.interfaces.ts
+++ src/productos/productos.interfaces.ts
@@ -1,2 +1,8 @@
 // exportar y crear la interfaces para producto la misma
 // se tiene que utilizar en el archivo productos.ts
+
+export interface Producto {
+    id: number;
+    nombre: string;
+    precio: number;
+  }
